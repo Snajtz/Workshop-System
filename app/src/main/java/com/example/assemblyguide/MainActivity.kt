18698856ec4c90ee261f4a559dd2cd9217cb5812
snajtz <erik.sneitz@gmail.com>
2019-10-15 17:38:36 +0200

    initial search layout

--- app/src/main/java/com/example/assemblyguide/MainActivity.kt
+++ app/src/main/java/com/example/assemblyguide/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.assemblyguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        textBox.setText("testText")
+
+        testButton.setOnClickListener {
+            textBox.setText("buttonPressed")
+
+        }
+
+
+
     }
+
 }
+
